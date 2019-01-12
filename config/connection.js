372cc11409280bdba62bb541a8178b7b8bca69df
MarkHays <mark.hays1991@gmail.com>
2019-01-11 18:58:42 -0800

    updating connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+connection = mysql.createConnection(procress.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
   host: "104.154.253.41",
   port: 3306,
   user: "josh",
   password: "josh",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
